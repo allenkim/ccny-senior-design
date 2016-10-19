f0ad416e914f0f8b6344833ceab94fcfdef2c59b
Allen Kim <allenkim95@gmail.com>
2016-10-19 00:55:13 -0400

    Update links with query

--- p1/allen/parse_articles.py
+++ p1/allen/parse_articles.py
@@ -1,4 +1,5 @@
 from newspaper import Article
+from random import randint
 
 with open("links.txt","r") as infile:
     links = infile.readlines()
@@ -14,8 +15,12 @@ with open("links_with_query.txt","a") as outfile:
             continue
         a.nlp()
         outfile.write(url)
-        outfile.write(a.title)
-        outfile.write(a.summary + "\n")
+        outfile.write(a.title + "\n")
+        summary = a.summary.splitlines()
+        if len(summary) == 0:
+            outfile.write("\n")
+        else:
+            outfile.write(summary[randint(0,len(summary)-1)] + "\n")
         outfile.write(str(a.keywords) + "\n\n")
         print("Processed " + str(count) + " articles")
         count += 1
